654f52b279e87e96bd823a3109e2701fc8f631d9
Guoqing Li <liguoqing5@xdf.cn>
2021-01-26 20:01:55 +0800

    删除不用的jar包
    Signed-off-by: Guoqing Li <liguoqing5@xdf.cn>

--- BuffaloAdSdkDemo/src/main/java/com/buffalo/ads/ui/AdViewConfigHelper.java
+++ BuffaloAdSdkDemo/src/main/java/com/buffalo/ads/ui/AdViewConfigHelper.java
@@ -3,7 +3,6 @@ package com.buffalo.ads.ui;
 import com.buffalo.ads.ui.impls.AdmobRenderAdapter;
 import com.buffalo.ads.ui.impls.FacebookRenderAdapter;
 import com.buffalo.ads.ui.impls.MopubRenderAdapter;
-import com.buffalo.ads.ui.impls.YahooRenderAdapter;
 import com.buffalo.adsdk.AdManager;
 import com.buffalo.adsdk.Const;
 
@@ -12,6 +11,5 @@ public class AdViewConfigHelper {
         AdManager.addRenderAdapter(Const.KEY_FB, new FacebookRenderAdapter(AdManager.getContext()));
         AdManager.addRenderAdapter(Const.KEY_AB, new AdmobRenderAdapter(AdManager.getContext()));
         AdManager.addRenderAdapter(Const.KEY_MP, new MopubRenderAdapter(AdManager.getContext()));
-        AdManager.addRenderAdapter(Const.KEY_YH, new YahooRenderAdapter(AdManager.getContext()));
     }
 }
